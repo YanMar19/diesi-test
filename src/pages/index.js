2c2338f012a0c39340f8e47f5e3ffe4982016633
Nick-X-Mar <47928530+Nick-X-Mar@users.noreply.github.com>
2023-09-05 17:03:20 +0300

    works

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import * as React from "react";
 import Layout from "../components/layout";
 import { StaticImage } from "gatsby-plugin-image";
 import Seo from "../components/seo";
+import Home from "./homepage";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>My first Home page using Gatsby</p>
       <StaticImage alt="Clifford" src="../images/nextusedcar.jpg" />
+      <Home />
     </Layout>
   );
 };
